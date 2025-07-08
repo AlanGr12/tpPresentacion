4d910b7e7ca52ab345cf0e9f1f3ffa50da555340
ALAN GABRIEL GORDON <48860730@ort>
2025-07-08 11:26:26 -0300

    j

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tpPresentacion.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
